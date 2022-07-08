f037da2d6dd10cce45398c7693802c480545b974
Jack Fenton <jack.fenton@cutover.com>
2022-07-08 15:52:40 +0100

    move bscscan check inside to handler to avoid duff caching

--- netlify/functions/bscscan/bscscan.js
+++ netlify/functions/bscscan/bscscan.js
@@ -6,21 +6,11 @@ const ethers = require('ethers')
 
 const BNBWallet = "0x749Ed5585af09f9bF60D5Fa29FdB9F7b8bC4e00F"
 
-const BUSDWallet = "0x749Ed5585af09f9bF60D5Fa29FdB9F7b8bC4e00F"
+// const BUSDWallet = "0x749Ed5585af09f9bF60D5Fa29FdB9F7b8bC4e00F"
 
 const BSCSCAN = process.env.BSCSCAN
 
 var HOLDINGS = 0
-
-const getApiData = async (resp, account) => {
-  for (let [, value] of Object.entries(resp.result)) {
-    if (value.from === account) {
-      let amount = parseFloat(ethers.utils.formatEther(value.value))
-      HOLDINGS = + amount
-    }
-  }
-  return HOLDINGS;
-};
 
 const CORS_HEADERS = {
   'Access-Control-Allow-Origin': '*',
@@ -37,6 +27,16 @@ exports.handler = async (event, context) => {
     }
   }
 
+  const getApiData = async (resp, account) => {
+    for (let [, value] of Object.entries(resp.result)) {
+      if (value.from === account) {
+        let amount = parseFloat(ethers.utils.formatEther(value.value))
+        HOLDINGS = + amount
+      }
+    }
+    return HOLDINGS;
+  };
+
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
@@ -46,22 +46,20 @@ exports.handler = async (event, context) => {
     let response = await fetch(`https://api.bscscan.com/api?module=account&action=txlist&address=${BNBWallet}&startblock=9000000&endblock=99999999&page=1&offset=1000&sort=asc&apikey=${BSCSCAN}`)
     let data = await response.json();
     let holdings = await getApiData(data, account);
-    response = await fetch(`https://api.bscscan.com/api?module=account&action=txlist&address=${BUSDWallet}&startblock=9000000&endblock=99999999&page=1&offset=1000&sort=asc&apikey=${BSCSCAN}`)
-    data = await response.json();
-    holdings = await getApiData(data, account);
     if (holdings > 0) {
-      console.log(data)
       console.log(holdings)
-      return { statusCode: 200, 
+      return {
+        statusCode: 200,
         headers: CORS_HEADERS,
-        body: "true" };
+        body: "true"
+      };
     }
     else {
-      console.log(data)
-      console.log(holdings)
-      return { statusCode: 200, 
+      return {
+        statusCode: 200,
         headers: CORS_HEADERS,
-        body: "false" };
+        body: "false"
+      };
     }
   } catch (error) {
     console.log(error);
